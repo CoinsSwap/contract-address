9d5a21287eec7d030cc875237f105613d3cf864d
vandeurenglenn <vandeurenglenn@gmail.com>
2021-02-21 16:30:43 +0100

    v0.1.0

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports {
+  1: {
+    zrxERC20Proxy: '0x95e6f48254609a6ee006f7d493c8e5fb97094cef',
+    zrxToken: '0xe41d2489571d322189246dafa5ebde1f4699f498',
+    zrxStakingProxy: '0xa26e80e7dea86279c6d778d702cc413e6cffa777',
+    zrxStaking: '0x2a17c35ff147b32f13f19f2e311446eeb02503f3',
+    weth: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+  },
+  42: {
+    zrxERC20Proxy: '0xaa460127562482faa5df42f2c39a025cd4a1cc0a',
+    zrxToken: '0x2002d3812f58e35f0ea1ffbf80a75a38c32175fa',
+    zrxStakingProxy: '0xe94cb304b3f515be7c95fedcfa249a84995fd748',
+    zrxStaking: '0x73ea24041e03a012c51a45c307e0ba376af0238c',
+    weth: '0xd0a1e359811322d97991e03f863a0c30c2cf029c'
+  }
+}
