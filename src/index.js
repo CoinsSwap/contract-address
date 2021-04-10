b6d73279d5b72dd73717c0dce5ce6b056a4b284c
vandeurenglenn <vandeurenglenn@gmail.com>
2021-04-10 15:46:38 +0200

    Fix uniswapV2Factory name

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ export default {
       uniswapV1Exchange: '0x9a256Dc79dc5f3aE303B453Dd19BfFc6c8aAaB94',
       uniswapV1Factory: '0x41a7C7d0B37db9f141062414Cbc0Ba4bB6E8ac7e',
       uniswapV2Router02: '0x147Ef17Df5204aC7E3193Eb49F2CEA75f9a3bE7B',
-      uniswapV2: '0xf53595F349Eb9ab2f3B09600618BAc92119Ba2cF',
+      uniswapV2Factory: '0xf53595F349Eb9ab2f3B09600618BAc92119Ba2cF',
     },
     zrx: {
       stakingProxy: '0xFf41bfb8289A655F9Afd10011D1D8178c77BE3f8',
